40f5efb427cd187e377e161db2b0bbb08ba7df2c
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-06-25 16:51:07 +0200

    Allineamento ECMState

--- arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/ECMState.java
+++ arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/ECMState.java
@@ -1,21 +1,17 @@
 package it.bmed.arch.uploadMulticanale.be.api;
 
+public enum ECMState {
 
-public enum ECMState{
-
-	INSERTED(1),PENDING(2),	MOVED(3), DELETED(4);
+	INSERTED(1), PENDING(2), MOVED(3), DELETED(4);
 
 	private Integer value;
-	
+
 	private ECMState(Integer value) {
-        this.value = value;
+		this.value = value;
 	}
-	
+
 	public Integer getValue() {
 		return value;
 	}
-	
-	
 
 }
-
