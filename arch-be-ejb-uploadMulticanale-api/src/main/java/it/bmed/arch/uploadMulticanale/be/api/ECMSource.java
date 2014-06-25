76183d1577c59de3dedd34359968fcf7fcc20e9a
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-06-25 16:50:21 +0200

    Allineamento ECMSource

--- arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/ECMSource.java
+++ arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/ECMSource.java
@@ -2,17 +2,16 @@ package it.bmed.arch.uploadMulticanale.be.api;
 
 public enum ECMSource {
 
-	NTERNET_BANKING(1),PORTALE_DI_SEDE(2),RETE_DI_VENDITA(3);
-	
+	INTERNET_BANKING(1), PORTALE_DI_SEDE(2), RETE_DI_VENDITA(3);
+
 	private Integer value;
-	
+
 	private ECMSource(Integer value) {
-        this.value = value;
+		this.value = value;
 	}
-	
+
 	public Integer getValue() {
 		return value;
 	}
 
 }
-
