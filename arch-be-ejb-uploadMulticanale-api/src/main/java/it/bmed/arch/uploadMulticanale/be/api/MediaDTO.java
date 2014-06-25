bc126333c8451c95797277eab95e6c64cd5fb953
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-06-25 16:48:20 +0200

    Allineamento MediaDTO

--- arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/MediaDTO.java
+++ arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/MediaDTO.java
@@ -1,128 +1,138 @@
 package it.bmed.arch.uploadMulticanale.be.api;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class MediaDTO implements Serializable {
 
-	private static final long	serialVersionUID	= 1227450135455809188L;
-	
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4352132526659585898L;
 	private String Canale;
 	private String ContainerType;
-	private Date DataInserimento;
 	private String idFileECM;
-	private Integer ECMType;
 	private String idUtente;
-	private String nomeApp;	
-	private String nomeFile;	
-	private String Sorgente_Path;	
-	private String Destinazione_Path;	
-	private String Stato;	
-	private String Tipo;	
-	private String tipoUtente;	
-	private String dominio;
-	private String idFile;
+	private String nomeApp;
+	private String nomeFile;
+	private String Sorgente_Path;
+	private String Destinazione_Path;
+	private String Tipo;
+	private String tipoUtente;
+	private Integer ECMType;
+	private Integer dominio;
+	private Integer Stato;
+	private Integer IdFile;
 
-	
-	/**
-	 * @return the idFile
-	 */
-	public String getIdFile() {
-		return idFile;
+	public Integer getIdFile() {
+		return IdFile;
 	}
-	/**
-	 * @param idFile the idFile to set
-	 */
-	public void setIdFile(String idFile) {
-		this.idFile = idFile;
+
+	public void setIdFile(Integer idFile) {
+		IdFile = idFile;
 	}
-	public String getCanale() {
-		return Canale;
-	}
-	public void setCanale(String canale) {
-		Canale = canale;
-	}
-	public String getContainerType() {
-		return ContainerType;
-	}
-	public void setContainerType(String containerType) {
-		ContainerType = containerType;
-	}
-	public Date getDataInserimento() {
-		return DataInserimento;
-	}
-	public void setDataInserimento(Date dataInserimento) {
-		DataInserimento = dataInserimento;
-	}
-	public String getIdFileECM() {
-		return idFileECM;
-	}
-	public void setIdFileECM(String idFileECM) {
-		this.idFileECM = idFileECM;
-	}
+
 	public Integer getECMType() {
 		return ECMType;
 	}
+
 	public void setECMType(Integer eCMType) {
 		ECMType = eCMType;
 	}
+
+	public Integer getStato() {
+		return Stato;
+	}
+
+	public void setStato(Integer stato) {
+		Stato = stato;
+	}
+
+	public Integer getDominio() {
+		return dominio;
+	}
+
+	public void setDominio(Integer dominio) {
+		this.dominio = dominio;
+	}
+
+	public String getCanale() {
+		return Canale;
+	}
+
+	public void setCanale(String canale) {
+		Canale = canale;
+	}
+
+	public String getContainerType() {
+		return ContainerType;
+	}
+
+	public void setContainerType(String containerType) {
+		ContainerType = containerType;
+	}
+
+	public String getIdFileECM() {
+		return idFileECM;
+	}
+
+	public void setIdFileECM(String idFileECM) {
+		this.idFileECM = idFileECM;
+	}
+
 	public String getIdUtente() {
 		return idUtente;
 	}
+
 	public void setIdUtente(String idUtente) {
 		this.idUtente = idUtente;
 	}
+
 	public String getNomeApp() {
 		return nomeApp;
 	}
+
 	public void setNomeApp(String nomeApp) {
 		this.nomeApp = nomeApp;
 	}
+
 	public String getNomeFile() {
 		return nomeFile;
 	}
+
 	public void setNomeFile(String nomeFile) {
 		this.nomeFile = nomeFile;
 	}
+
 	public String getSorgente_Path() {
 		return Sorgente_Path;
 	}
+
 	public void setSorgente_Path(String sorgente_Path) {
 		Sorgente_Path = sorgente_Path;
 	}
+
 	public String getDestinazione_Path() {
 		return Destinazione_Path;
 	}
+
 	public void setDestinazione_Path(String destinazione_Path) {
 		Destinazione_Path = destinazione_Path;
 	}
-	public String getStato() {
-		return Stato;
-	}
-	public void setStato(String stato) {
-		Stato = stato;
-	}
+
 	public String getTipo() {
 		return Tipo;
 	}
+
 	public void setTipo(String tipo) {
 		Tipo = tipo;
 	}
+
 	public String getTipoUtente() {
 		return tipoUtente;
 	}
+
 	public void setTipoUtente(String tipoUtente) {
 		this.tipoUtente = tipoUtente;
 	}
-	public String getDominio() {
-		return dominio;
-	}
-	public void setDominio(String dominio) {
-		this.dominio = dominio;
-	}
-	
-	
-	
 
 }
