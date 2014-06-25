1f5936a83b6faae1178b63cc6c811f219c34cff2
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-06-25 11:45:07 +0200

    Move DTO

---
+++ arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/MoveDTO.java
@@ -0,0 +1,20 @@
+package it.bmed.arch.uploadMulticanale.be.api;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Request DTO for Move operation
+ * @author donatello.boccaforno
+ *
+ */
+public class MoveDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4035052721772232075L;	
+	// TODO: add private instance variables
+	// TODO: add getters
+	// TODO: add setters
+}
