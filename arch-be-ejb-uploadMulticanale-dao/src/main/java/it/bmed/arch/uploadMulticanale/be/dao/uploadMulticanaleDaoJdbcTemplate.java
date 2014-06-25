015309fb3948020b9c58191cb305a4d8f4af6653
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-06-25 16:57:11 +0200

    Allineamento uploadMulticanaleDaoJdbcTemplate

--- arch-be-ejb-uploadMulticanale-dao/src/main/java/it/bmed/arch/uploadMulticanale/be/dao/uploadMulticanaleDaoJdbcTemplate.java
+++ arch-be-ejb-uploadMulticanale-dao/src/main/java/it/bmed/arch/uploadMulticanale/be/dao/uploadMulticanaleDaoJdbcTemplate.java
@@ -1,19 +1,25 @@
 package it.bmed.arch.uploadMulticanale.be.dao;
 
-import it.bmed.arch.uploadMulticanale.be.api.*;
-import it.bmed.asia.exception.*;
-
+import it.bmed.arch.uploadMulticanale.be.api.MediaRequest;
+import it.bmed.arch.uploadMulticanale.be.api.MediaResponse;
+import it.bmed.arch.uploadMulticanale.be.api.UpdateMediaRequest;
+import it.bmed.asia.exception.ApplicationException;
 
 /**
  * The uploadMulticanale DAO for JDBC access
  * 
- * @author generated automaticallu by bmed-asia-be-archetype 
+ * @author generated automaticallu by bmed-asia-be-archetype
  * 
  */
 public interface uploadMulticanaleDaoJdbcTemplate {
 
-	public MediaResponse InsertMedia(MediaRequest request) throws ApplicationException, Exception;
-	public MediaResponse listMedia( MediaRequest request) throws ApplicationException, Exception;		
-	public boolean updateMedia( UpdateMediaRequest request) throws ApplicationException,Exception;
-	
+	public MediaResponse insertMedia(MediaRequest request)
+			throws ApplicationException, Exception;
+
+	public MediaResponse listMedia(MediaRequest request)
+			throws ApplicationException, Exception;
+
+	public boolean updateMedia(UpdateMediaRequest request)
+			throws ApplicationException, Exception;
+
 }
