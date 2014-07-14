96af6a0bb8c5f32e81edd2a3086a241f9fde8204
Donatello Boccaforno <donatello.boccaforno@consulenti.mediolanum.it>
2014-07-14 14:05:36 +0200

    check parameters refactoring

--- arch-be-ejb-uploadMulticanale-service/src/main/java/it/bmed/arch/uploadMulticanale/be/service/UploadMulticanaleServiceImpl.java
+++ arch-be-ejb-uploadMulticanale-service/src/main/java/it/bmed/arch/uploadMulticanale/be/service/UploadMulticanaleServiceImpl.java
@@ -38,86 +38,12 @@ public class UploadMulticanaleServiceImpl implements UploadMulticanaleService,
 	}
 
 	@Override
-	public ECMResponse insertMedia(ECMRequest request)
-			throws TechnicalException, Exception {
+	public ECMResponse insertMedia(ECMRequest request) throws TechnicalException, Exception {
 		try {
-//			MediaDTO dto = request.getMediaDTO();
-			ECMFile ecmFile = request.getEcmFile(); 
-			// stato e dominio per il db devono essere not null
-			// default
-			if(ecmFile.getState() == null /*|| ecmFile.getState() == 0*/) {
-
-				ecmFile.setState(ECMState.INSERTED);
-			}
-
-			// default perche obbligatorio sul db ma non obbligatorio in input
-			if (ecmFile.getEcmType() == null /*|| ecmFile.getECMType() == 0*/) {
-				ecmFile.setEcmType(ECMType.IBM_FILENET);
-			}
-
-			// campi non obbligatori lasciati null
-			// controllo obbligatorieta dei campiù
-
-			if (ecmFile == null || ecmFile.getChannel().isEmpty()
-					|| ecmFile.getUserId().isEmpty()
-					|| ecmFile.getNameApp().isEmpty()
-					|| ecmFile.getNameFile().isEmpty()
-					|| ecmFile.getSourcePath().isEmpty()
-					|| ecmFile.getUserType().isEmpty()
-					|| ecmFile.getSource() == null
-					|| ecmFile.getType().isEmpty()) {
-				log.debug("insertMedia: parametri non valorizzati.");
-
-				IErrorCode er = UploadMulticanaleErrorCodeEnums
-						.valueOf("TCH_GENERIC_ERROR");
-				TechnicalException tec = new TechnicalException(er);
-
-				throw tec;
-			}
-
-			// controllo lunghezza del campo di input stato
-			if (ecmFile.getChannel().length() > 15
-					|| ecmFile.getContainerType().length() > 20
-					|| ecmFile.getDestinationPath().length() > 200
-					|| ecmFile.getIdFileECM().length() > 500
-					|| ecmFile.getUserId().length() > 16
-					|| ecmFile.getNameApp().length() > 100
-					|| ecmFile.getNameFile().length() > 50
-					|| ecmFile.getSourcePath().length() > 500
-					|| ecmFile.getType().length() > 5
-					|| ecmFile.getUserType().length() > 50) {
-				log.debug("insertMedia: parametri non corretti, qualche campo troppo lungo");
-				IErrorCode er = UploadMulticanaleErrorCodeEnums
-						.valueOf("TCH_GENERIC_ERROR");
-				TechnicalException tec = new TechnicalException(er);
-
-				throw tec;
-
-			}
-
-			/*
-			 * 
-			 * COD_UPLD_FILE_INTERN INTEGER NOT NULL , ID_FILE VARCHAR2(50) NOT
-			 * NULL ENABLE, COD_TIPO_ECM INTEGER NULL , ECM_TYPE VARCHAR2(3) NOT
-			 * NULL ENABLE, COD_TIPO_PROVNZ_FILE INTEGER NOT NULL , DOMINIO
-			 * VARCHAR2(50), COD_STATO_ECM INTEGER NOT NULL , STATO VARCHAR2(3),
-			 * 
-			 * DES_APPLICNE VARCHAR2(100) NULL , NOME_APP VARCHAR2(100),
-			 * DES_DEST_PATH VARCHAR2(200) NULL , DESTINAZIONE_PATH
-			 * VARCHAR2(500), DEN_CNTR VARCHAR2(50) NULL , CONTAINER_TYPE
-			 * VARCHAR2(50), COD_TIPO_CANA VARCHAR2(15) NULL , CANALE
-			 * VARCHAR2(15), COD_TIPO_UTE VARCHAR2(50) NULL , TIPO_UTENTE
-			 * VARCHAR2(15), DEN_FILE VARCHAR2(50) NULL , NOME_FILE
-			 * VARCHAR2(50), DES_SORG_PATH VARCHAR2(200) NULL , SORGENTE_PATH
-			 * VARCHAR2(500), COD_UPLD_FILE_ECM VARCHAR2(500) NULL , ID_FILE_ECM
-			 * VARCHAR2(200), DEN_ESTNS_FILE VARCHAR2(5) NULL , TIPO_FILE
-			 * VARCHAR2(25), GSTD_M_NOM_ULT_MODF CHAR(16) NULL, GSTD_X_USER
-			 * CHAR(16) NULL, GSTD_D_ULT_MODF_RECORD DATE , DATA_AGGIORNAMENTO
-			 * DATE DEFAULT sysdate, GSTD_D_INS_RECORD DATE DEFAULT sysdate
-			 * NULL, DATA_INSERIMENTO LONG, GSTD_X_TIP_MODF CHAR(1) NULL,
-			 * GSTD_F_ESIST CHAR(1) DEFAULT 'S' NOT NULL CHECK (GSTD_F_ESIST IN
-			 * ('S','N')) ) ;
-			 */
+			// check required parameters
+			checkRequiredParametersInsertMedia(request);
+			// check parameters max length
+			checkParametersLengthInsertMedia(request);
 
 			log.debug(" insertMedia: parametri  corretti.");
 			ECMResponse response = new ECMResponse();
@@ -135,53 +61,13 @@ public class UploadMulticanaleServiceImpl implements UploadMulticanaleService,
 	}
 
 	@Override
-	public ECMResponse listMedia(ECMRequest request)
-			throws TechnicalException, Exception {
+	public ECMResponse listMedia(ECMRequest request) throws TechnicalException, Exception {
 		try {
 			
-			ECMFile ecmFile = request.getEcmFile();
-			if (ecmFile == null) {
-				log.error("listMedia: ECMFile argument cannot be null.");
-				IErrorCode er = UploadMulticanaleErrorCodeEnums.valueOf("TCH_GENERIC_ERROR");
-				TechnicalException tec = new TechnicalException(er);
-				throw tec;				
-			}
-
-			/*
-			 * tolgo obbligatorieta per tutti
-			 * 
-			 * if (dto == null || dto.getCanale().isEmpty() ||
-			 * dto.getIdUtente().isEmpty() || dto.getTipoUtente().isEmpty() ) {
-			 * log.debug("Errore InsertMedia: parametri non corretti ");
-			 * 
-			 * IErrorCode er =
-			 * UploadMulticanaleErrorCodeEnums.valueOf("TCH_GENERIC_ERROR");
-			 * TechnicalException tec = new TechnicalException( er ); throw tec;
-			 * }
-			 */
-
-			// controllo lunghezza del campo di input stato			
-			if (ecmFile.getChannel() == null || ecmFile.getChannel().length() > 15) {
-				parameterError("channel");
-			} 
+			checkRequiredParametersListMedia(request);
 			
-			if (ecmFile.getUserId() == null || ecmFile.getUserId().length() > 16) {
-				parameterError("userId");
-			}
-			
-			if (ecmFile.getUserType() == null || ecmFile.getUserType().length() > 50) {
-				parameterError("userType");
-			}
-			
-//			if (ecmFile.getIdFile() == null || ecmFile.getIdFile() == 0) {
-//				parameterError("idFile");
-//			}
-			
-			
-			if (ecmFile.getIdFileECM() == null || ecmFile.getIdFileECM().length() > 500) {
-				parameterError("idFileECM");
-			}
-			
+			checkParametersLengthListMedia(request);
+						
 			ECMResponse response = new ECMResponse();
 			response = uploadMulticanaleDaoJdbcTemplate.listMedia(request);
 
@@ -200,43 +86,16 @@ public class UploadMulticanaleServiceImpl implements UploadMulticanaleService,
 	}
 
 	@Override
-	public boolean updateMedia(UpdateECMRequest request) throws SystemFault,
-			Exception {
+	public boolean updateMedia(UpdateECMRequest request) throws TechnicalException, Exception {
 		try {
 			boolean res;
 
-			String Destinazione_path = request.getDestinationPath();
-			String idFileECM = request.getIdFileECM();
-			String Container_type = request.getContainerType();
-			String nomApp = request.getNameApp();
-
 			// controllo campi obbligatori
-			if ((request.getIdFile() == null || request.getIdFile() == 0)
-					&& (request.getState() == null)
-					&& request.getNameApp().isEmpty()) {
-				log.debug("Errore updateMedia: IdFile obbligatorio ");
-
-				IErrorCode er = UploadMulticanaleErrorCodeEnums
-						.valueOf("TCH_GENERIC_ERROR");
-				TechnicalException tec = new TechnicalException(er);
-
-				throw tec;
-			}
+			checkRequiredParametersUpdateMedia(request);
 
 			// controllo lunghezza del campo di input stato
-			if (Container_type.length() > 20
-					|| Destinazione_path.length() > 200
-					|| idFileECM.length() > 500 || nomApp.length() > 100) {
-
-				log.debug("Errore updateMedia: campi non corretti per dimensione ");
-				// Il valore della correttezza del valore va fatta a livello
-				// superiore
-				IErrorCode er = UploadMulticanaleErrorCodeEnums
-						.valueOf("TCH_GENERIC_ERROR");
-				TechnicalException tec = new TechnicalException(er);
-				throw tec;
-			}
-
+			checkParametersLengthUpdateMedia(request);
+			
 			res = uploadMulticanaleDaoJdbcTemplate.updateMedia(request);
 			return res;
 
@@ -252,9 +111,220 @@ public class UploadMulticanaleServiceImpl implements UploadMulticanaleService,
 		}
 
 	}
+	
+	/**
+	 * Check where required parameters are null and throws TechnicalException
+	 * @param request as <b>UpdateECMRequest</b>
+	 * @throws TechnicalException
+	 */
+	private void checkRequiredParametersInsertMedia(ECMRequest request) throws TechnicalException {
+		final String methodName = "checkRequiredParametersInsertMedia";
+		final String cause = "Parameter cannot be null.";
+		
+		if(request == null) {
+			parameterError(methodName, "ECMRequest", cause);
+		}
+		
+		ECMFile ecmFile = request.getEcmFile();
+		if(ecmFile == null) {
+			parameterError("checkRequiredParametersInsertMedia", "ECMFile", cause);
+		}
+		
+		if(ecmFile.getChannel() == null) {	// length is not checked here
+			parameterError(methodName, "channel", cause);
+		}
+		
+		if (ecmFile.getUserId() == null) {
+			parameterError(methodName, "userId", cause);
+		}
+		
+		if (ecmFile.getNameApp() == null) {
+			parameterError(methodName, "nameApp", cause);
+		}
+		
+		if (ecmFile.getNameFile() == null) {
+			parameterError(methodName, "nameFile", cause);
+		}
+		
+		if (ecmFile.getSourcePath() == null ) {
+			parameterError(methodName, "sourcePath", cause);
+		}
+		
+		if (ecmFile.getSource() == null) {
+			parameterError(methodName, "source", cause);
+		}
+		
+		if (ecmFile.getState() == null) {
+			parameterError(methodName, "state", cause);
+		}		
+	}
+	
+	/**
+	 * Check where required parameters exceed max length and throws TechnicalException 
+	 * @param request
+	 * @throws TechnicalException
+	 */
+	private void checkParametersLengthInsertMedia(ECMRequest request) throws TechnicalException {
+		final String method = "checkParametersLengthInsertMedia";
+		final String emptyCause = "Parameter cannot be empty.";
+		final String exeedCause = "Parameter max length exceeded.";
+		
+		ECMFile ecmFile = request.getEcmFile();
+		
+		// Required field
+		if (ecmFile.getChannel().isEmpty()) {
+			parameterError(method, "channel", emptyCause);
+		}
+		if (ecmFile.getChannel().length() > 15) {
+			parameterError(method, "channel", exeedCause);
+		}
+		
+		// Not required field		
+//		if (ecmFile.getContainerType().isEmpty()) {
+//			parameterError(method, "containerType", emptyCause);
+//		}		
+		if (ecmFile.getContainerType() != null && ecmFile.getContainerType().length() > 20) {
+			parameterError(method, "containerType", exeedCause);
+		}
+		
+		if (ecmFile.getIdFileECM() != null && ecmFile.getIdFileECM().length() > 500) {
+			parameterError(method, "idFileECM", exeedCause);
+		}
+		
+		if (ecmFile.getUserId().isEmpty()) {
+			parameterError(method, "userId", emptyCause);
+		}
+		if (ecmFile.getUserId().length() > 16) {
+			parameterError(method, "userId", exeedCause);
+		}
+		
+		if(ecmFile.getNameApp().isEmpty()) {
+			parameterError(method, "nameApp", emptyCause);
+		}
+		if(ecmFile.getNameApp().length() > 100) {
+			parameterError(method, "nameApp", exeedCause);
+		}
+		
+		if(ecmFile.getNameFile().isEmpty()) {
+			parameterError(method, "nameFile", emptyCause);
+		}
+		if(ecmFile.getNameFile().length() > 100) {
+			parameterError(method, "nameFile", exeedCause);
+		}
+		
+		if(ecmFile.getSourcePath() != null && ecmFile.getSourcePath().length() > 500) {
+			parameterError(method, "sourcePath", exeedCause);
+		}
+		
+		if(ecmFile.getDestinationPath() != null && ecmFile.getDestinationPath().length() > 200) {
+			parameterError(method, "destinationPath", exeedCause);
+		}		
+				
+	}
+	
+	/**
+	 * Check where required parameters exceed max length and throws TechnicalException 
+	 * @param request
+	 * @throws TechnicalException
+	 */
+	private void checkParametersLengthListMedia(ECMRequest request) throws TechnicalException {
+		final String method = "checkParametersLengthListMedia";
+		final String cause = "Parameter max length exceeded.";
+		ECMFile ecmFile = request.getEcmFile();
+			
+		if (ecmFile.getChannel() != null && ecmFile.getChannel().length() > 15) {
+			parameterError(method, "channel", cause);
+		} 
+		
+		if (ecmFile.getUserId() != null && ecmFile.getUserId().length() > 16) {
+			parameterError(method, "userId", cause);
+		}
+		
+		if (ecmFile.getUserType() != null && ecmFile.getUserType().length() > 50) {
+			parameterError(method, "userType", cause);
+		}		
+		
+		if (ecmFile.getIdFileECM() != null && ecmFile.getIdFileECM().length() > 500) {
+			parameterError(method, "idFileECM", cause);
+		}
+	}
 
-	private void parameterError(String parameter) throws TechnicalException {
-		log.error("listMedia: " + parameter +" parameter error.");
+	/**
+	 * Check where required parameters are null and throws TechnicalException
+	 * @param request as <b>UpdateECMRequest</b>
+	 * @throws TechnicalException
+	 */
+	private void checkRequiredParametersListMedia(ECMRequest request) throws TechnicalException {
+		final String method = "checkRequiredParametersListMedia";
+		final String cause = "Parameter cannot be null.";
+		
+		if(request == null) {
+			parameterError(method, ECMRequest.class.getName(), cause);
+		}
+		
+		if(request.getEcmFile() == null) {
+			parameterError(method, ECMFile.class.getName(), cause);
+		}
+		
+		
+	}
+	
+	/**
+	 * Check where required parameters exceed max length and throws TechnicalException 
+	 * @param request
+	 * @throws TechnicalException
+	 */
+	private void checkParametersLengthUpdateMedia(final UpdateECMRequest request) throws TechnicalException {
+		final String methodNAme = "checkParametersLengthUpdateMedia";
+		final String cause = "Parameter max length exceeded.";
+	
+		if(request.getIdFileECM().length() > 500) {
+			parameterError(methodNAme, "idFileECM", cause);
+		}
+		
+		if(request.getContainerType().length() > 20) {
+			parameterError(methodNAme, "ContainerType", cause);
+		}
+		
+		if (request.getDestinationPath().length() > 200) {
+			parameterError(methodNAme, "DestinationPath", cause);
+		}
+		
+		if (request.getNameApp().length() > 100) {
+			parameterError(methodNAme, "NAmeApp", cause);
+		}					
+	}
+	
+	/**
+	 * Check where required parameters are null and throws TechnicalException
+	 * @param request as <b>UpdateECMRequest</b>
+	 * @throws TechnicalException
+	 */
+	private void checkRequiredParametersUpdateMedia(UpdateECMRequest request) throws TechnicalException {
+		final String methodName = "checkRequiredParametersUpdateMedia";
+		final String cause = "Parameter cannot be null.";
+		
+		if (request.getIdFile() == null || request.getIdFile() <= 0) {
+			parameterError(methodName, "idFile", cause);
+		}
+		
+		if (request.getState() == null ) {
+			parameterError(methodName, "Sate", cause);
+		}
+		
+		if (request.getNameApp().isEmpty()) {
+			parameterError(methodName, "NameApp", cause);
+		}
+	}
+	
+	/**
+	 * Logs and throws TechnocalExceptions, based on called method and parameter name
+	 * @param method as <b>String</b>
+	 * @param parameter as <b>String</b> 
+	 * @throws TechnicalException
+	 */
+	private void parameterError(String method, String parameter, String cause) throws TechnicalException {
+		log.error(method + ": " + parameter +" parameter error. " + cause);
 		IErrorCode er = UploadMulticanaleErrorCodeEnums.valueOf("TCH_GENERIC_ERROR");
 		TechnicalException tec = new TechnicalException(er);
 		throw tec;
