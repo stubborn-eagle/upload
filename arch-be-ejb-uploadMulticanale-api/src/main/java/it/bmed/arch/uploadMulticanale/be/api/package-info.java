c1539a541a9e7ed063f0c8cd36c2a2eff0e545ad
alessandro.marini <amarini@iad2.it>
2016-06-01 16:20:55 +0200

    Adeguamento attributeFormDefault QUALIFIED

---
+++ arch-be-ejb-uploadMulticanale-api/src/main/java/it/bmed/arch/uploadMulticanale/be/api/package-info.java
@@ -0,0 +1,5 @@
+@javax.xml.bind.annotation.XmlSchema(
+		namespace = "http://UploadMulticanaleDomApp.service.bmed.it/v1", 
+		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, 
+		attributeFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package it.bmed.arch.uploadMulticanale.be.api;
